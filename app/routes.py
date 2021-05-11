f3caa2e1d50593ddb049b2b4720bd4dce48e7ec4
Chris McAnally <chris@adadevelopersacademy.org>
2021-05-11 10:36:01 -0700

    customers and videos index routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+from app import db
+import os
+
+videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+@videos_bp.route("", methods=["GET"])
+def videos_index():
+    return jsonify([]), 200
+
+
+@customers_bp.route("", methods=["GET"])
+def customers_index():
+    return jsonify([]), 200
