061115452980540dfe9e6a839ad57678f3d16ba5
Chris McAnally <chris@adadevelopersacademy.org>
2021-05-11 17:30:38 -0700

    invalid video and customer data functions

--- app/routes.py
+++ app/routes.py
@@ -9,10 +9,14 @@ videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 
 def invalid_video_data(video_json):
-    return "name" not in video_json or  \
-        "postal_code" not in video_json or \
-        "phone" not in video_json
+    return "title" not in video_json or  \
+        "release_date" not in video_json or \
+        "total_inventory" not in video_json
 
+def invalid_customer_data(customer_json):
+    return "name" not in customer_json or  \
+        "postal_code" not in customer_json or \
+        "phone" not in customer_json
 
 @videos_bp.route("", methods=["GET"])
 def videos_index():
