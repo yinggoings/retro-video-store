b07eef440c52f9065c3bb6f568939e523d7bd728
Chris M <chris@adadevelopersacademy.org>
2021-05-13 12:45:28 -0700

    Revert "Cm/solution"

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-db = SQLAlchemy()
-migrate = Migrate()
-
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.url_map.strict_slashes = False
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
-    else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
-
-    
-    # import models for Alembic Setup
-    from app.models.customer import Customer
-    from app.models.video import Video
-    from app.models.rental import Rental
-
-    # Setup DB
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    from .routes import videos_bp, customers_bp
-    app.register_blueprint(videos_bp)
-    app.register_blueprint(customers_bp)
 
     return app
