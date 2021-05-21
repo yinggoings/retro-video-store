14f0c1938ca95cfd0811725122c4bbf484edcadd
Chris McAnally <chris@adadevelopersacademy.org>
2021-05-21 11:07:06 -0700

    fix missing imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
