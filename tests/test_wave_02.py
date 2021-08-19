9e3a4c68b5a24ad6ba888c337b71ab422e002d06
Becca Elenzil <becca@adadevelopersacademy.org>
2021-08-19 15:58:13 -0700

    get all rentals by video test

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -1,6 +1,16 @@
 from app.models.video import Video
 from app.models.customer import Customer
 
+VIDEO_TITLE = "A Brand New Video"
+VIDEO_ID = 1
+VIDEO_INVENTORY = 1
+VIDEO_RELEASE_DATE = "01-01-2001"
+
+CUSTOMER_NAME = "A Brand New Customer"
+CUSTOMER_ID = 1
+CUSTOMER_POSTAL_CODE = "12345"
+CUSTOMER_PHONE = "123-123-1234"
+
 def test_checkout_video(client, one_video, one_customer):
 
     response = client.post("/rentals/check-out", json={
@@ -56,3 +66,13 @@ def test_checkin_video_not_checked_out(client, one_video, one_customer):
     response_body == {"message": "No outstanding rentals for customer # 1 and video 1"}
     
 
+def test_rentals_by_video(client, one_checked_out_video):
+    response = client.get("/videos/1/rentals")
+
+    response_body = response.get_json()
+
+    response.status == 200
+    len(response_body) == 1
+    response_body[0]["name"] == CUSTOMER_NAME
+
+#def test_rentals_by_customer(client, one_checked_out_video):
