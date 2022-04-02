5b81e4e5319db1eeeb21c5b2e1b0e77ba2ca98d4
Ying G <yingg@yings-air.lan>
2022-04-01 21:14:04 -0700

    fix Rental model

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,5 +1,6 @@
 from app import db
 
 class Rental(db.Model):
-    customer_id = db.Column(db.Integer,db.ForeignKey(customer.id),primary_key=True,nullable=False)
-    video_id = db.Column(db.Integer,db.ForeignKey(video.id),priamry_key=True,nullable=False)
+    customer_id = db.Column(db.Integer,db.ForeignKey('customer.id'),primary_key=True,nullable=False)
+    video_id = db.Column(db.Integer,db.ForeignKey('video.id'),priamry_key=True,nullable=False)
+    
